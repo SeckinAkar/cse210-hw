241a4c0218977ee1b20c232ad15e64d921c22737
Seckin Akar <seckinakar09@gmail.com>
2023-11-05 00:32:39 +0300

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        // Console.WriteLine(number);
+        
+        bool userGuess = false;
+
+        while (userGuess == false)
+        {
+            Console.Write("What is the magic number? ");
+            string stringGuess = Console.ReadLine();
+            int guess = int.Parse(stringGuess);
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+                userGuess = true;
+            }
+        }
+
     }
 }
