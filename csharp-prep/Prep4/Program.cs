a971c327963cb8f322c31e58c3f2eaf079599150
Seckin Akar <seckinakar09@gmail.com>
2023-11-05 01:14:18 +0300

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+
+        List<int> numbers = new List<int>();
+
+        int userGuess = -1;
+        int sum = 0;
+
+        while (userGuess != 0)
+        {
+            Console.Write("Enter a number: ");
+            
+            string userResponse = Console.ReadLine();
+            userGuess = int.Parse(userResponse);
+            
+            if (userGuess != 0)
+            {
+                numbers.Add(userGuess);
+            }
+        }
+        
+        // Finding sum:
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Finding average:
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Finding the minimum number:
+        int min = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The smallest number is: {min}");
     }
 }
